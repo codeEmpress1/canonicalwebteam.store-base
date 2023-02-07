0ec410731162feb5ff6f6841e77df93bf750a0e3
codeEmpress1 <alimot.akinbode@canonical.com>
2023-02-07 09:03:25 +0300

    Add config (#3)
    * minimal config
    
    * add basic config
    
    * rename variables and files
    
    * update readme

---
+++ canonicalwebteam/store_base/app.py
@@ -0,0 +1,28 @@
+"""
+Extensible flask application base for all stores.
+
+It gets response from the store_api and it extends each store blueprint to make a complete application
+"""
+from canonicalwebteam.flask_base.app import FlaskBase
+import canonicalwebteam.store_base.utils.config as config
+from canonicalwebteam.store_base.utils.extensions import csrf
+from canonicalwebteam.store_base.sample_blueprint.views import test_bp
+
+
+"""
+
+config would be passed in at store level
+
+a default config is supplied.
+"""
+def create_app(app_name, store_bp=test_bp, testing=False):
+
+    app = FlaskBase(__name__, app_name)
+
+    app.register_blueprint(store_bp)
+    app.config.from_object(config)
+    app.testing = testing
+
+    csrf.init_app(app)
+
+    return app
