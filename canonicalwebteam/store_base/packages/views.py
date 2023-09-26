9d4dac7bd181b456729e077f4f7f0ae39a85c956
ALIMOT AKINBODE <halof.halima@gmail.com>
2023-09-26 10:16:48 +0300

    pass login-required decorator from app

--- canonicalwebteam/store_base/packages/views.py
+++ canonicalwebteam/store_base/packages/views.py
@@ -5,7 +5,6 @@ from flask import (
     request,
     session,
     make_response,
-    current_app as app,
 )
 
 from canonicalwebteam.store_base.packages.logic import (
@@ -15,94 +14,95 @@ from canonicalwebteam.store_base.packages.logic import (
 )
 from canonicalwebteam.store_base.utils.config import PACKAGE_PARAMS
 
-from canonicalwebteam.store_base.utils.decorators import login_required
 
-store_packages = Blueprint(
-    "package",
-    __name__,
-)
-
-
-@store_packages.route("/store.json")
-def get_store_packages():
-    app_name = app.name
-    filters = dict(request.args)
-    filters.pop("page", {})
-    params = PACKAGE_PARAMS[app_name]
-    store, fields, size = params["store"], params["fields"], params["size"]
-
-    page = int(request.args.get("page", 1))
-
-    res = make_response(
-        get_packages(store, app_name, fields, size, page, filters)
+def init_packages(app):
+    store_packages = Blueprint(
+        "package",
+        __name__,
     )
-    return res
+    login_required = app.config["LOGIN_REQUIRED"]
 
+    @store_packages.route("/store.json")
+    def get_store_packages():
+        app_name = app.name
+        filters = dict(request.args)
+        filters.pop("page", {})
+        params = PACKAGE_PARAMS[app_name]
+        store, fields, size = params["store"], params["fields"], params["size"]
 
-@store_packages.route("/<any(charms, bundles, snaps):package_type>")
-@login_required
-def package(package_type):
-    """
-    Retrieves and returns package information based on the current app
-    and package type.
+        page = int(request.args.get("page", 1))
 
-    :returns: Response: The HTTP response containing the JSON data of the
-    packages.
-    """
-
-    app_name = app.name
-    publisher = PACKAGE_PARAMS[app_name]["publisher"]
-
-    publisher_api = publisher(talisker.requests.get_session())
-
-    if app_name.startswith("charmhub"):
-        publisher_packages = publisher_api.get_account_packages(
-            session["account-auth"], "charm", include_collaborations=True
+        res = make_response(
+            get_packages(store, app_name, fields, size, page, filters)
         )
-        page_type = request.path[1:-1]
+        return res
 
-        response = make_response(
-            {
-                "published_packages": [
-                    package
-                    for package in publisher_packages
-                    if package["status"] == "published"
-                    and package["type"] == page_type
-                ],
-                "registered_packages": [
-                    package
-                    for package in publisher_packages
-                    if package["status"] == "registered"
-                    and package["type"] == page_type
-                ],
-                "page_type": page_type,
-            }
-        )
-        return response
+    @store_packages.route("/<any(charms, bundles, snaps):package_type>")
+    @login_required
+    def package(package_type):
+        """
+        Retrieves and returns package information based on the current app
+        and package type.
 
-    if app_name.startswith("snapcraft"):
-        account_info = publisher_api.get_account(flask.session)
+        :returns: Response: The HTTP response containing the JSON data of the
+        packages.
+        """
 
-        user_snaps, registered_snaps = get_snaps_account_info(account_info)
-        flask_user = flask.session["publisher"]
+        app_name = app.name
+        publisher = PACKAGE_PARAMS[app_name]["publisher"]
 
-        response = make_response(
-            {
-                "snaps": user_snaps,
-                "current_user": flask_user["nickname"],
-                "registered_snaps": registered_snaps,
-            }
-        )
+        publisher_api = publisher(talisker.requests.get_session())
 
-        return response
+        if app_name.startswith("charmhub"):
+            publisher_packages = publisher_api.get_account_packages(
+                session["account-auth"], "charm", include_collaborations=True
+            )
+            page_type = request.path[1:-1]
 
+            response = make_response(
+                {
+                    "published_packages": [
+                        package
+                        for package in publisher_packages
+                        if package["status"] == "published"
+                        and package["type"] == page_type
+                    ],
+                    "registered_packages": [
+                        package
+                        for package in publisher_packages
+                        if package["status"] == "registered"
+                        and package["type"] == page_type
+                    ],
+                    "page_type": page_type,
+                }
+            )
+            return response
 
-@store_packages.route("/<package_name>/card.json")
-def get_store_package(package_name):
-    app_name = app.name
+        if app_name.startswith("snapcraft"):
 
-    params = PACKAGE_PARAMS[app_name]
-    store, fields = params["store"], params["fields"]
+            account_info = publisher_api.get_account(flask.session)
 
-    res = make_response(get_package(store, app_name, package_name, fields))
-    return res
+            user_snaps, registered_snaps = get_snaps_account_info(account_info)
+            flask_user = flask.session["publisher"]
+
+            response = make_response(
+                {
+                    "snaps": user_snaps,
+                    "current_user": flask_user["nickname"],
+                    "registered_snaps": registered_snaps,
+                }
+            )
+
+            return response
+
+    @store_packages.route("/<package_name>/card.json")
+    def get_store_package(package_name):
+        app_name = app.name
+
+        params = PACKAGE_PARAMS[app_name]
+        store, fields = params["store"], params["fields"]
+
+        res = make_response(get_package(store, app_name, package_name, fields))
+        return res
+
+    app.register_blueprint(store_packages)
